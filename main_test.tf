defa844e6631a60a8b0f109db1dc904709c06f50
Akshay444Kumar <aryakshay444@gmail.com>
2024-06-20 17:54:02 +0530

    Completed Terraform Variables tutorial

---
+++ main_test.tf
@@ -0,0 +1,30 @@
+# test/main_test.tf
+
+# Define a test block
+/*
+test "example_test" {
+    
+  # Define variable directly within the test file
+  variable "instance_type" {
+    default = "t2.small"
+  }
+
+  run "run_block_1" {
+    # Define variable within a run block
+    variable "instance_type" {
+      default = "t2.micro"
+    }
+    
+    # Actual test steps (e.g., terraform apply, terraform plan, assertions)
+  }
+
+  run "run_block_2" {
+    # Another run block, different value for instance_type
+    variable "instance_type" {
+      default = "t3.medium"
+    }
+    
+    # Actual test steps (e.g., terraform apply, terraform plan, assertions)
+  }
+}
+*/
--- terraform.tf
+++ terraform.tf
@@ -1,22 +1,22 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
+  # Copyright (c) HashiCorp, Inc.
+  # SPDX-License-Identifier: MPL-2.0
 
-terraform {
+  terraform {
 
-  /* Uncomment this block to use Terraform Cloud for this tutorial
-  cloud {
-      organization = "organization-name"
-      workspaces {
-        name = "learn-terraform-variables"
-      }
-  }
-  */
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.42.0"
+    // Uncomment this block to use Terraform Cloud for this tutorial
+    cloud {
+        organization = "akshay-terraform-org"
+        workspaces {
+          name = "learn-terraform-variables"
+        }
     }
+    
+    required_providers {
+      aws = {
+        source  = "hashicorp/aws"
+        version = "~> 5.42.0"
+      }
+    }
+    required_version = ">= 1.7.5, < 2.0.0"
   }
-  required_version = "~> 1.7.5"
-}
 
